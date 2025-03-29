ef539ac3749bd06acc0842c017594522715787fc
Mashfiqun Nabi <mashnabi99@gmail.com>
2025-03-29 12:53:19 +0600

    bugfix

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def copy_files_recursive(source_dir_path, dest_dir_path):
 
 dir_path_static = "./static"
 dir_path_public = "./docs"
-
+basepath = "/"
 
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
